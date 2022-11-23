65bded24fafc453c75f5494ba8fa697db7418134
DariaBV <dreamhappy8@gmail.com>
2022-11-23 17:59:52 +0100

    Fixed js

--- js/countdown.js
+++ js/countdown.js
@@ -2,7 +2,7 @@ const someDate = new Date('November 30 2022 00:00:00');
 const currentDate = new Date();
 let startTime = (someDate - currentDate) / 1000;
 
-const countDownEl = document.querySelector('.countdown')
+// const countDownEl = document.querySelector('.countdown')
 const launchDays = document.querySelector('.launch-days')
 const launchHors = document.querySelector('.launch-hours')
 const launchMinutes = document.querySelector('.launch-minutes')
@@ -14,6 +14,7 @@ function updateCountDown() {
     let hours = Math.floor(startTime / 60 / 60 - days * 24);
     let minutes = Math.floor(startTime / 60 - days * 24 * 60 - hours * 60);
     let seconds = Math.floor(startTime - days * 24 * 60 * 60 - hours * 60 * 60 - minutes * 60);
+    days = days < 10 ? '0' + days : days;
     hours = hours < 10 ? '0' + hours : hours;
     minutes = minutes < 10 ? '0' + minutes : minutes;
     seconds = seconds < 10 ? '0' + seconds : seconds;
